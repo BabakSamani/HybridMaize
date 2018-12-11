5e750e38479af1fe7d811ace3677060f1ae91adf
BabakSamani <BabakSamani@users.noreply.github.com>
2018-12-10 22:23:41 -0600

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
-	{{--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-2uqBhq1_iCmt0OFu7eLEbZlHIVl2Ckk&libraries=places"--}}
+	{{--<script src="https://maps.googleapis.com/maps/api/js?key=<KEY>&libraries=places"--}}
 		{{--type="text/javascript"></script>--}}
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
